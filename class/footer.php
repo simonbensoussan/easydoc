6309f9c2470443087c915262a9978e71212e4a0d
simonbensoussan <simonbensoussan.sb@gmail.com>
2016-05-19 14:25:11 +0200

    delete

--- class/footer.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar navbar-default navbar-fixed-bottom">
-  <div class="container">
-  	<p class = "navbar-text pull-left">Site built by Simon Bensoussan 2016</p>
-   <a href="http://www.gedstore.fr/" target='_blank'><button type="button" class="btn btn-danger navbar-btn navbar-right"> GEDSTORE</button></a>
-  </div>
-</nav>
